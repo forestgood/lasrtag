ac21a63c5a06ac81933f6222ca6014baa2505355
forestgood <fgoodiu@gmail.com>
2013-03-25 12:08:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lasrtag::Application.config.secret_token = '1ef49849784c5a39a8b441c5a4f831a9ccae9dc46738d46df8ac1bfe5fbba5001f906eb7e5fc344f6085dd78421ef40275db720011faa6cc9a774fb6b9f753a2'
